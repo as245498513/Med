2190904c10026eac2e1a86311b46b47dccacd1af
as245498513 <Meckel_cmj@163.com>
2017-12-01 00:20:23 +0800

    clear

--- application/User/Lang/zh-cn/setting.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-		"OLD_PASSWORD" => '原始密码',
-		"NEW_PASSWORD" => '新密码',
-		"CONFIRM_PASSWORD" => '重复新密码',
-		
-);
--- data/lang/moerduo/Lang/zh-cn/admin_menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'MOERDUO_AUDIO_INDEX' => '音频上传',
-  'MOERDUO_MED_REPORT' => '报表中心',
-);
